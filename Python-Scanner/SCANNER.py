f8c1da1fd20688f39e66a523f6d369b2e291f811
Kronos24 <54737933+Kronos24@users.noreply.github.com>
2021-09-23 23:34:39 +0300

    Add files via upload

---
+++ Python-Scanner/SCANNER.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+hedefip = input("Hedef İp Adresini Giriniz: ")
+os.system("apt-get install figlet")
+os.system("apt-get install knockpy")
+os.system("apt-get install gobuster")
+os.system("apt-get install nikto")
+os.system("apt-get install nmap")
+os.system("clear")
+os.system("figlet NMAP")
+print("""
+
+
+""")
+os.system("nmap -sV " + hedefip + " -T4 --script=vuln")
+print("""
+
+
+""")
+os.system("figlet GOBUSTER")
+print("""
+
+
+""")
+os.system("gobuster dir -u http://" + hedefip + " -w /usr/share/dirb/wordlists/common.txt")
+print("""
+
+
+""")
+os.system("figlet KNOCKPY")
+print("""
+
+
+""")
+os.system("knockpy " + hedefip)
+print("""
+
+
+""")
+os.system("figlet NIKTO")
+print("""
+
+
+""")
+os.system("nikto -h " + hedefip)
